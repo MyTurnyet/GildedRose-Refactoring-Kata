0243315ed0e1a4dd18203badb3f7b70745e67ece
paige.watson <paige.watson@salesforce.com>
2025-12-01 10:08:56 -0800

    clean up imports

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,8 +3,6 @@ package com.gildedrose;
 import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class GildedRoseTest {
 
 
