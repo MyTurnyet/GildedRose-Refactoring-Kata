e8e91ea722b84897021c5369b345cebeec803286
paige.watson <paige.watson@salesforce.com>
2025-12-01 08:39:33 -0800

    add all other combinations

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,9 +12,11 @@ class GildedRoseTest {
     void testAllCombinations() {
         String[] names = {
                 "Aged Brie",
-                
+                "Backstage passes to a TAFKAL80ETC concert",
+                "Sulfuras, Hand of Ragnaros",
+                "Normal Item"
         };
-        Integer[] sellInValues = {-1, 0, 2, 6,5,10, 11};
+        Integer[] sellInValues = {-1, 0, 2, 6, 5, 10, 11};
         Integer[] qualityValues = {0, 1, 49, 50};
         CombinationApprovals.verifyAllCombinations(
                 (name, sellIn, quality) ->
