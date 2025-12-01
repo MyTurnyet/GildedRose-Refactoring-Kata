f93d68a094b9b5311a997f4a14b2baa097ef3b83
paige.watson <paige.watson@salesforce.com>
2025-12-01 08:37:06 -0800

    add SellIn values

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,15 +14,16 @@ class GildedRoseTest {
                 "Aged Brie",
                 
         };
+        Integer[] sellInValues = {-1, 0, 2, 6,5,10, 11};
         CombinationApprovals.verifyAllCombinations(
-                (name) ->
+                (name, sellIn) ->
                 {
-                    Item item = new Item(name, 0, 0);
+                    Item item = new Item(name, sellIn, 0);
                     Item[] itemArray = new Item[]{item};
                     GildedRose app = new GildedRose(itemArray);
                     app.updateQuality();
                     return String.format("[%s, %d, %d]", item.name, item.sellIn, item.quality);
-                }, names);
+                }, names, sellInValues);
     }
 
 }
